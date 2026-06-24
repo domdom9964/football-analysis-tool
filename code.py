43b96e3092a3e2b240287d65f531051936e32d6d
domdom9964 <domdom9964@web.de>
2026-06-24 17:38:06 +0200

    Enhance Player class with market value attribute
    Added market_value attribute to Player class and updated related methods. Modified main menu to include market value management.

--- code.py
+++ code.py
@@ -1,25 +1,30 @@
+import json
+import os
 
+Save_file = "squad.json"
 # All perdefined lists in the code
 
 class Player():
-    def __init__(self, name, club, position, age, goals, assists, matches, rating):
+    def __init__(self, name, club, position, age, goals, assists, matches, rating, market_value):
         self.name = name
         self.club = club
         self.position = position 
         self.age = age
-        self. goals = goals
-        self. assists = assists 
+        self.goals = goals
+        self.assists = assists 
         self.matches = matches
         self.rating = rating
+        self.market_value = market_value
     
     def print_player_card(self):
 
         print("=" * 45)                        
         print(f"  {self.name} — {self.club}")
-        print(f"  Position : {self.position}  |  Age: {self.age}")
-        print(f"  Matches  : {self.matches}")
-        print(f"  Goals    : {self.goals}   Assists: {self.assists}")
-        print(f"  Rating   : {self.rating:.1f} / 10")
+        print(f"  Position    : {self.position}  |  Age: {self.age}")
+        print(f"  Matches     : {self.matches}")
+        print(f"  Goals       : {self.goals}   Assists: {self.assists}")
+        print(f"  Rating      : {self.rating:.1f} / 10")
+        print(f"  Market Value: {self.market_value}M")
         print("=" * 45)
 
 
@@ -51,24 +56,65 @@ class Player():
             return "Average"
         else:                               
             return "Needs Improvement"
-
         
-squad = [
-    Player("Erling Haaland", "Man City", "Forward", 23, 36, 8, 35, 9.1), 
-    Player("Marcus Rashford", "Man United", "Forward", 26, 17, 5, 32, 7.4),
-    Player("Kevin De Bruyne", "Man City", "Midfielder", 32, 7, 16, 28, 8.8),
-    Player("Jude Bellingham", "Real Madrid", "Midfielder", 20, 23, 12, 38, 9.0),
-    Player("Virgil van Dijk", "Liverpool", "Defender", 32, 4, 2, 36, 8.3),
-    Player("Trent Alexander-Arnold", "Liverpool", "Defender", 25, 3, 13, 34, 8.1),
-    Player("Alisson Becker", "Liverpool", "Goalkeeper", 31, 0, 0, 36, 8.5),
+    def to_dict(self):
+        return{
+            "name": self.name,
+            "club": self.club,
+            "position": self.position,
+            "age": self.age,
+            "goals": self.goals,
+            "assists": self.assists,
+            "matches": self.matches,
+            "rating": self.rating,
+            "market_value": self.market_value,
+        }
+        
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            data["name"],
+            data["club"],
+            data["position"],
+            data["age"],
+            data["goals"],
+            data["assists"],
+            data["matches"],
+            data["rating"],
+            data["market_value"]
+        )
+        
+        
+starting_squad = [
+    Player("Erling Haaland", "Man City", "Forward", 23, 36, 8, 35, 9.1, 200), 
+    Player("Marcus Rashford", "Man United", "Forward", 26, 17, 5, 32, 7.4, 40),
+    Player("Kevin De Bruyne", "Man City", "Midfielder", 32, 7, 16, 28, 8.8, 10),
+    Player("Jude Bellingham", "Real Madrid", "Midfielder", 20, 23, 12, 38, 9.0, 160),
+    Player("Virgil van Dijk", "Liverpool", "Defender", 32, 4, 2, 36, 8.3, 15),
+    Player("Trent Alexander-Arnold", "Liverpool", "Defender", 25, 3, 13, 34, 8.1, 60),
+    Player("Alisson Becker", "Liverpool", "Goalkeeper", 31, 0, 0, 36, 8.5, 15),
 ]     
 
 
 
 # All definitions in the Code
 
+def save_squad(squad):
+    data = [player.to_dict() for player in squad]
+    with open(Save_file, "w") as file:
+        json.dump(data, file, indent=4)
+    print(f"\n Squad saved to {Save_file}\n")
 
-
+def load_squad():
+    if not os.path.exists(Save_file):
+        return starting_squad
+    with open(Save_file, "r") as file:
+        data = json.load(file)
+    squad = [Player.from_dict(p) for p in data]
+    print(f"\nSquad loaded from {Save_file}\n")
+    return squad
+        
+        
 def squad_stats(squad):
     total_players = len(squad)
 
@@ -84,7 +130,7 @@ def squad_stats(squad):
     top_scorer = max(squad, key=lambda p: p.goals)
     top_assist = max(squad, key=lambda p: p.assists)
     top_rated  = max(squad, key=lambda p: p.rating)
-
+    most_expensive = max(squad, key=lambda p: p.market_value)
 
     avg_rating = round(sum(p.rating for p in squad) / total_players, 2)
     print(f"\nTotal Players: {total_players}")
@@ -104,13 +150,14 @@ def squad_stats(squad):
         "top_scorer": top_scorer,
         "top_assist": top_assist,
         "top_rated": top_rated,
+        "most_expensive":  most_expensive,
     }
    
 
 
-def add_player(squad, name, club, position, age, goals, assists, matches, rating):
+def add_player(squad, name, club, position, age, goals, assists, matches, rating, market_value):
 
-    new_player = Player(name, club, position, age, goals, assists, matches, rating)
+    new_player = Player(name, club, position, age, goals, assists, matches, rating, market_value)
     squad.append(new_player)      
     print(f"\n✅ {name} has been added to the squad!\n")#
     
@@ -187,9 +234,25 @@ def compare_players(squad, name1, name2):
     print(f"  {player1.name} is rated: {player1.rate_player()}")   # reusing rate_player
     print(f"  {player2.name} is rated: {player2.rate_player()}")
     print("=" * 55 + "\n")
+    
+def change_market_value(squad, player1):
+    for player in squad:
+        if player.name.lower() == player1.lower():
+            new_market_value = int(input(f"Market Value (in M):"))
+            player.market_value = new_market_value
+            print(f"\n {player.name} has a new Market Value of {player.market_value}M")
+            save_squad(squad)
+            return
+
+        
+        
+    
         
         
 def main_menu():
+    
+    squad = load_squad()
+    
     while True:
         print("\nMain Menu:")
         print("1. view full squad")
@@ -198,9 +261,11 @@ def main_menu():
         print("4. view players by position")
         print("5. compare two players")
         print("6. delete a player")
-        print("7. exit")
+        print("7. save squad")
+        print("8. change Market value")
+        print("9. exit")
             
-        choice = input("Enter your choice (1-7):")
+        choice = input("Enter your choice (1-9):")
             
         if choice == "1":
             print("\nFULL SQUAD\n")
@@ -219,7 +284,8 @@ def main_menu():
             assists = int(input(f"Assists:"))
             matches = int(input(f"Matches:"))
             rating = float(input(f"Rating:"))
-            add_player(squad, name, club, position, age, goals, assists, matches, rating)
+            market_value =int(input(f"Market Value:"))
+            add_player(squad, name, club, position, age, goals, assists, matches, rating, market_value)
         elif choice == "4":
             print (f"1: Forward\n 2: Middfielder\n 3: Defender\n 4: Goalkeeper")
             position = str(input(f"\nPosition (1-4) :"))
@@ -236,7 +302,7 @@ def main_menu():
         elif choice == "5":
             print("\nCompare Players\n")
             for player in squad:
-                print(f"  - {player.name}")   # show squad so user knows available names
+                print(f"  - {player.name}")  
             name1 = input("\nEnter first player name: ")
             name2 = input("Enter second player name: ")
             compare_players(squad, name1, name2)
@@ -247,6 +313,16 @@ def main_menu():
             name = input("\nPlayer to remove from the squad: ")
             remove_player(squad, name)
         elif choice == "7":
+            save_squad(squad)
+            print(f"Squad has been succesfully saved")
+        elif choice == "8":
+            print(f"\nChange Market Value")
+            for player in squad:
+                print(f"-{player.name}")
+            player = input(f"choose a Player to change the Market value:")
+            change_market_value(squad, player)
+        elif choice == "9":
+            save_squad(squad)
             print("Exiting. Goodbye!")
             break
         else:
@@ -255,4 +331,5 @@ def main_menu():
 # The actual code
 
 main_menu()
+
 
